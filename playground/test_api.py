9f8b4a0bf916581224ce3ec7f44a5e114bbca034
Itu Alexandru-Mihail <alexandru.itu@gmail.com>
2024-04-21 07:32:43 +0300

    removed credentials

--- playground/test_api.py
+++ playground/test_api.py
@@ -1,4 +1,4 @@
 import requests
-headers = {"X-User-id": "6278", "X-User-hash":"a235942bff158e1027d455a9d36d2fa3"}
+headers = {"X-User-id": "", "X-User-hash":""}
 response = requests.get('https://data.uradmonitor.com/api/v1/devices',headers = headers)
 print(response.content)
